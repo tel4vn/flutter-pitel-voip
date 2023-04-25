455752bed041ea722f0840dcdeb0190c46e689e1
anhquangmobile <quangduong@tel4vn.com>
2023-04-25 15:23:36 +0700

    feat: export lib flutter_pitel_voip

---
+++ lib/flutter_pitel_voip.dart
@@ -0,0 +1,22 @@
+library flutter_pitel_voip;
+
+// Export push notif
+export './voip_push/voip_notif.dart';
+export './voip_push/push_notif.dart';
+
+// Pitel sdk
+export './pitel_sdk/pitel_call.dart';
+export './pitel_sdk/pitel_client.dart';
+
+// Pitel service
+export './services/pitel_service.dart';
+export './services/sip_info_data.dart';
+
+// Component
+export './component/app_life_cycle/app_life_cycle.dart';
+export './component/pitel_call_state.dart';
+export './component/sip_pitel_helper_listener.dart';
+export './component/pitel_rtc_video_view.dart';
+
+//SIP
+export './sip/src/sip_ua_helper.dart';
