f1ee000361d0e98366b91c56351ffef574243d30
anhquangmobile <quangduong@pitel.vn>
2025-05-27 17:01:46 +0700

    build: rc

--- lib/config/pitel_config.dart
+++ lib/config/pitel_config.dart
@@ -3,8 +3,11 @@ class PitelConfigure {
   static String get domainSDK => 'https://sdkdemo.tel4vn.com';
   static String get domainPortal => 'https://portal.tel4vn.com';
   //! PROD
-  // static String get apiPushUrl => 'https://api-pusher.tel4vn.com/v1';
-  static String get apiPushUrl => 'https://api.pitel-pusher.dev.tel4vn.com/v1';
+  // static String get apiPushUrl => 'https://api-pusher.tel4vn.com/v1'; // PROD
+  static String get apiPushUrl =>
+      'https://api.pitel-pusher.tel4vn.com/v1'; // RC
+  // static String get apiPushUrl =>
+  //     'https://api.pitel-pusher.dev.tel4vn.com/v1'; // DEV
   static String get token => 'Pitel@@2023!Mobile!PitelConnect';
   static String get API_KEY => _apiKey;
   static bool isDebug = true;
