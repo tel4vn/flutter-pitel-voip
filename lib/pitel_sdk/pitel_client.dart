02871108a4c7a6fbb6a30c7e78a6fa65e10ba8e9
anhquangmobile <quangduong@pitel.vn>
2025-12-09 10:12:46 +0700

    refactor: improve content-length handling in SIP message parsing

--- lib/pitel_sdk/pitel_client.dart
+++ lib/pitel_sdk/pitel_client.dart
@@ -96,13 +96,11 @@ class PitelClient {
     //settings.webSocketSettings.userAgent = 'Dart/2.8 (dart:io) for OpenSIPS.';
     settings.uri = 'sip:$_username@${_sipServer?.domain}:${_sipServer?.port}';
     settings.contactUri =
-        'sip:$_username@${_sipServer?.domain}:${_sipServer?.port};pn-prid=${pnPushParams.pnPrid};pn-provider=${pnPushParams.pnProvider};pn-param=${pnPushParams.pnParam};fcm-token=${pnPushParams.fcmToken};transport=wss;name-caller=encode';
+        'sip:$_username@${_sipServer?.domain}:${_sipServer?.port};pn-prid=${pnPushParams.pnPrid};pn-provider=${pnPushParams.pnProvider};pn-param=${pnPushParams.pnParam};fcm-token=${pnPushParams.fcmToken};transport=wss';
     settings.webSocketSettings.extraHeaders = _wsExtraHeaders;
     settings.authorizationUser = _username;
     settings.password = _password;
-    final bytes = utf8.encode(_displayName);
-    final base64Str = base64.encode(bytes);
-    settings.displayName = "${base64Str}pitelsdkencode";
+    settings.displayName = _displayName;
     settings.userAgent = _userAgent;
     settings.register_expires = 600;
     settings.dtmfMode = DtmfMode.RFC2833;
