903e1a7a8aa922e557855646b94943a1259f18f1
anhquangmobile <quangduong@pitel.vn>
2026-01-10 09:53:27 +0700

    fix(wip): end call

--- lib/services/pitel_service.dart
+++ lib/services/pitel_service.dart
@@ -8,12 +8,15 @@ import 'package:flutter_pitel_voip/flutter_pitel_voip.dart';
 /// This class provides concrete implementations for Pitel VoIP service operations
 /// and listens to SIP helper events.
 class PitelServiceImpl implements PitelService, SipPitelHelperListener {
+  static final PitelServiceImpl _instance = PitelServiceImpl._internal();
+  factory PitelServiceImpl() => _instance;
+
   final pitelClient = PitelClient.getInstance();
 
   SipInfoData? sipInfoData;
 
   /// Creates an instance of [PitelServiceImpl] and registers as a listener.
-  PitelServiceImpl() {
+  PitelServiceImpl._internal() {
     pitelClient.pitelCall.addListener(this);
   }
 
@@ -92,5 +95,10 @@ class PitelServiceImpl implements PitelService, SipPitelHelperListener {
     if (kDebugMode) {
       print('❌ ❌ ❌ transportStateChanged ${state.state.toString()}');
     }
+  }
+
+  @override
+  void dispose() {
+    pitelClient.pitelCall.removeListener(this);
   }
 }
--- lib/services/pitel_service_interface.dart
+++ lib/services/pitel_service_interface.dart
@@ -12,4 +12,5 @@ abstract class PitelService {
   Future<PitelSettings> registerSipWithoutFCM(
     PnPushParams pnPushParams,
   );
+  void dispose();
 }
